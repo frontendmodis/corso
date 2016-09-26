82d68ddb5b0f2a275a5d8f0aa73a75ef10d53296
encelado748 <vinnocenti@outlook.it>
2016-09-26 10:46:50 -0700

    Added Scarpe class

---
+++ Projects/MyLinq/MyLinq/Scarpe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLinq
+{
+    public class Scarpe
+    {
+        public int Id { get; set; }
+        public string Colore { get; set; }
+        public string Marca { get; set; }
+        public decimal Prezzo { get; set; }
+        public Tipo Tipo { get; set; }
+    }
+
+    public enum Tipo : byte
+    {
+        Ginnastica, Decollete, Ballerina,
+        Stringate, Tacco12, Bikers, Sneakers,
+        Sandali, Infradito, Stivali
+    }
+}
