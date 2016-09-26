53d0df19e6681daf925521d81692e1711911b684
encelado748 <vinnocenti@outlook.it>
2016-09-26 10:55:34 -0700

    Use of Where and lambda expression

--- Projects/MyLinq/MyLinq/Program.cs
+++ Projects/MyLinq/MyLinq/Program.cs
@@ -10,6 +10,36 @@ namespace MyLinq
     {
         static void Main(string[] args)
         {
+            var scarpe = new List<Scarpe>()
+            {
+                new Scarpe { Id = 1, Colore = "Rosso",  Marca="Cinti",      Tipo = Tipo.Stivali,   Prezzo = 130m  },
+                new Scarpe { Id = 2, Colore = "Blu",    Marca="Armani",     Tipo = Tipo.Stringate, Prezzo = 980m  },
+                new Scarpe { Id = 3, Colore = "Nero",   Marca="Valentino",  Tipo = Tipo.Infradito, Prezzo = 210m  },
+                new Scarpe { Id = 4, Colore = "Beige",  Marca="Balenciaga", Tipo = Tipo.Decollete, Prezzo = 1030m  },
+                new Scarpe { Id = 5, Colore = "Giallo", Marca="Armani",     Tipo = Tipo.Sandali,   Prezzo = 581m  }
+            };
+
+
+            //var risultato = scarpe.Where(delegate (Scarpe s)
+            //{
+            //    var isArmani = false;
+            //    isArmani = s.Marca == "Armani";
+            //    return isArmani;
+            //});
+
+            //var risultato = scarpe.Where(delegate (Scarpe s)
+            //{
+            //    return s.Marca == "Armani";
+            //});
+
+            var risultato = scarpe.Where(s => s.Marca == "Armani");
+
+            foreach(var s in risultato)
+            {
+                Console.WriteLine("La scarpa {0} è di Armani", s.Id);
+            }
+
+            Console.ReadLine();
         }
     }
 }
