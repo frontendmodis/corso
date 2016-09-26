53d10a7baf2a2614c22364d35b679ce3730492b9
encelado748 <vinnocenti@outlook.it>
2016-09-26 11:12:19 -0700

    More Linq Examples

--- Projects/MyLinq/MyLinq/Program.cs
+++ Projects/MyLinq/MyLinq/Program.cs
@@ -39,6 +39,46 @@ namespace MyLinq
                 Console.WriteLine("La scarpa {0} è di Armani", s.Id);
             }
 
+            risultato = scarpe.Where(s => s.Prezzo > 500 && s.Tipo == Tipo.Decollete);
+
+            foreach (var s in risultato)
+            {
+                Console.WriteLine("La scarpa {0} è una decollete che costa più di 500 euro.", s.Id);
+            }
+
+            // Se non la trova va in errore
+            var scarpa = scarpe.First(s => s.Id == 1);
+
+            // Se non la trova da null
+            scarpa = scarpe.FirstOrDefault(s => s.Id == 20);
+
+            // Va in errore se la quantità è diversa da 1
+            scarpa = scarpe.Single(s => s.Id == 1);
+
+            // Va in errore se la quantità è maggiore di 1
+            scarpa = scarpe.SingleOrDefault(s => s.Id == 1);
+
+            // Concatenazione
+            scarpa = scarpe
+                .Where(s => s.Prezzo > 100)
+                .FirstOrDefault(s => s.Colore == "Rosso");
+
+            // Prime 3 scarpe
+            risultato = scarpe.Take(3);
+
+            // Prime 4 dopo aver saltalto la prima
+            risultato = scarpe.Skip(1).Take(4);
+
+            // Ordinamento
+            risultato = scarpe
+                .OrderBy(s => s.Prezzo)
+                .ThenBy(s => s.Colore)
+                .ThenByDescending(s => s.Id);
+
+            var valoreTotale = scarpe.Sum(s => s.Prezzo);
+
+            var colori = scarpe.Select(s => s.Colore).Distinct().OrderBy(c => c);
+
             Console.ReadLine();
         }
     }
