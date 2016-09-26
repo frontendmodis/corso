eae50c9064b4aee11437c290c165f5eea0e6736e
encelado748 <vinnocenti@outlook.it>
2016-09-26 10:32:57 -0700

    Added lambda expression

--- Projects/Lambda/Lambda/Program.cs
+++ Projects/Lambda/Lambda/Program.cs
@@ -15,10 +15,11 @@ namespace Lambda
             Console.WriteLine("Inizio lavoro...");
             //lavoro.Fai(new FaccioUnGrossoLavoro.Avanzamento(Scrivo));
             //lavoro.Fai(Scrivo);
-            lavoro.Fai(delegate (decimal p)
-            {
-                Console.Write("\r{0}%   ", p);
-            });
+            //lavoro.Fai(delegate (decimal p)
+            //{
+            //    Console.Write("\r{0}%   ", p);
+            //});
+            lavoro.Fai(p => Console.Write("\r{0}%   ", p));
 
             Console.WriteLine("\n...fine lavoro.");
             Console.ReadLine();
