3306580f05c117aa8234735b663af572bcb45dfb
encelado748 <vinnocenti@outlook.it>
2016-10-19 09:39:33 -0700

    ToDo Use of Modernizr

--- Javascript/ToDo/script.js
+++ Javascript/ToDo/script.js
@@ -1,4 +1,4 @@
-(function(){
+$(function(){
 "use strict";
 
 init();
@@ -6,115 +6,78 @@ init();
 function init(){
     load();
 
-    var myForm = document.getElementById('myForm');
-    myForm.addEventListener('submit', myFormSubmitted);
+    $('#myForm').submit(myFormSubmitted);
+    $('#myReset').click(function(e){    
+        $('.done').remove();
+    });
 
-    var myReset = document.getElementById('myReset');
-    myReset.addEventListener('click', myResetClicked);
-    
+    if (Modernizr.mutationobserver) {
+        MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+        var obs = new MutationObserver(function(mutation, observer){
+            localStorage.setItem('todo', $('#myList').html());
+        });
+
+        obs.observe($('#myList')[0], {
+            subtree: true,
+            attributes: true,
+            childList: true
+        });
+    } else {
+        $.getScript('mutationevents.js');
+    }
 }
 
 function myFormSubmitted(e){
     e.preventDefault();
 
-    var input = document.getElementById('todo');
-    var li = document.createElement('li');
-    li.innerHTML = input.value;
-    li.setAttribute('draggable', 'true');
-    addLiListener(li);  
+    var $input = $('#todo');
+    var $li = $('<li></li>')
+        .attr('draggable', 'true')
+        .html($input.val());
 
-    var myList = document.getElementById('myList');
-    myList.appendChild(li);
-    save();
+    addLiListener($li);  
 
-    input.value = '';
+    $('#myList').append($li);
+
+    $input.val('');
 }
 
-function myResetClicked(e){
-    var fatti = document.getElementsByClassName('done');
-    var myList = document.getElementById('myList');
-
-    for(var i = fatti.length - 1; i >= 0; i--){
-        myList.removeChild(fatti[i]);
-    } 
-    save();
-}
-
-function liClicked(){
-    if(this.className == 'done'){
-        this.className = '';
-    } else {
-        this.className = 'done';
-    }
-    save();
-}
-
-function save(){
-    var myList = document.getElementById('myList');
-    localStorage.setItem('todo', myList.innerHTML);
-}
-
-function load(){
+function load(){    
     var todos = localStorage.getItem('todo');
-    var myList = document.getElementById('myList');
-    myList.innerHTML = todos;
-
-    var listItem = myList.childNodes;
-    for(var i = 0; i < listItem.length; i++){
-            addLiListener(listItem[i]);                     
-    }
+    $('#myList').html(todos);
+    $('#myList').children().each(function(i, li){
+        addLiListener($(li));
+    });
 }
 
-function addLiListener(li){
-    li.addEventListener('click', liClicked);
-    li.addEventListener('dragstart', drag);
-    li.addEventListener('drop', drop);
-    li.addEventListener('dragover', dragOver);                                     
+function addLiListener($li){
+    $li.on({
+        'click': function(){
+            $(this).toggleClass('done');
+       },
+        'dragstart': function(e){
+            var index = $('li').index(e.target);        
+            e.originalEvent.dataTransfer.setData("text/plain", index);
+        },
+        'drop': drop,
+        'dragover': function(e){
+            e.preventDefault();
+        }
+    });
 }            
-
-function getLiArray()
-{
-    var nodeList = [];
-    var nodeCollection = document.getElementsByTagName('li');
-    var length = nodeCollection.length;
-    var i;
-
-    for(i = 0; i < length; i++){
-        nodeList.push(nodeCollection[i]);
-    }
-    
-    return nodeList;
-
-    // Oppure più brevemente ma meno chiaramente:
-    //return Array.prototype.slice.call(document.getElementsByTagName('li'));
-}
-
-function drag(e){
-    var nodeList = getLiArray();
-    var index = nodeList.indexOf(e.target);        
-    e.dataTransfer.setData("text/plain", index);
-}
 
 function drop(e){
     e.preventDefault();
-    var nodeList = getLiArray();                
-    var ixO = parseInt(e.dataTransfer.getData("text"));
-    var origin = nodeList[ixO];
+    var ixO = parseInt(e.originalEvent.dataTransfer.getData("text"));
+    var $origin = $('li').eq(ixO);
     var destination = e.target;
 
     var destinationContent = destination.innerHTML;
     var destinationClass = destination.className;
 
-    destination.innerHTML = origin.innerHTML;
-    destination.className = origin.className;
-    origin.innerHTML = destinationContent;
-    origin.className = destinationClass;
-    
-    save();
+    destination.innerHTML = $origin.html();
+    destination.className = $origin.attr('class');
+    $origin.html(destinationContent);
+    $origin.attr('class', destinationClass);    
 }
-
-function dragOver(e){
-    e.preventDefault();
-}
-
-})();
+});
